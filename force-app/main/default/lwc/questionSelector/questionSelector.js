80899d54fce3bf0d1639906e3e789e7156d8991d
ashish1553 <62050867+ashish1553@users.noreply.github.com>
2020-05-04 13:08:06 +0530

    unnessary button removed

--- force-app/main/default/lwc/questionSelector/questionSelector.js
+++ force-app/main/default/lwc/questionSelector/questionSelector.js
@@ -1,16 +1,44 @@
 import { LightningElement, track } from 'lwc';
+import searchQueries from '@salesforce/apex/questionController.searchQueries';
+
+const columns = [
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Question', fieldName: 'Question__c' },
+    { label: 'Country', fieldName: 'Country__c' },
+    { label: 'Region', fieldName: 'Region__c' }
+];
 
 export default class questionSelector extends LightningElement {
     
     @track countryValue;
     @track regionValue;
-
+    @track data = [];
+    @track columns = columns;
+    @track values;
+    
     handleData(event){
         this.countryValue = event.detail.country;
         this.regionValue = event.detail.region;
         
+        searchQueries({"regionKey": this.regionValue}).then(result => { 
+            this.values = result;
+            this.error = undefined;
+            console.log('result');
+            console.log(result);
+            
+            
+        })
+        .catch(error => { 
+            this.values = undefined;
+            this.error = error;
+            console.log('error');
+            
+        })
+        console.log('hell');
 
     }
+
+
 
 
 }
